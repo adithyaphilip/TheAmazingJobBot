f3cc0d4a2e0b98a9847aa912a418ff1699239c64
Adithya Philip <t-adphi@microsoft.com>
2018-07-25 20:08:43 +0530

    added interfaces

---
+++ app/src/main/java/com/job/theamazingjobbot/bot/BotResponse.java
@@ -0,0 +1,9 @@
+package com.job.theamazingjobbot.bot;
+
+import android.view.View;
+
+public interface BotResponse {
+    public View GetView();
+
+    public String GetText();
+}
---
+++ app/src/main/java/com/job/theamazingjobbot/bot/ConversationBot.java
@@ -0,0 +1,5 @@
+package com.job.theamazingjobbot.bot;
+
+public interface ConversationBot {
+    public String GetMessage(String userMessage);
+}
